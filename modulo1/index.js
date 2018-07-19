301c8529edb0395198535e21e69c0f704d898b6b
eudalio <tonnydallios@gmail.com>
2018-07-19 09:37:35 -0300

    Configurando o ESLint

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 nunjucks.configure('views', {
-    autoscape: true,
-    express: app
+  autoscape: true,
+  express: app,
 });
 
-app.set('view engine', 'njk')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'njk');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index', { nomes: ['Eudálio Sousa', 'Bruno', 'Patrick'] })
+app.get('/', (_req, res) => {
+  res.render('index', { nomes: ['Eudálio Sousa', 'Bruno', 'Patrick'] });
 });
 
 app.post('/auth', (req, res) => {
-    const { username, password } = req.body
-    res.send(`Você logou com o usuário: ${username} e senha ${password}`)
-})
+  const { username, password } = req.body;
+  res.send(`Você logou com o usuário: ${username} e senha ${password}`);
+});
 
-app.listen(3000)
+app.listen(3000);
