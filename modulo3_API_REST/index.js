3d8c4b4564f9436bf672f14007c6504361087537
eudalio <tonnydallios@gmail.com>
2018-08-05 17:27:24 -0300

    Adicionando body-parser e Insomnia

--- modulo3_API_REST/index.js
+++ modulo3_API_REST/index.js
@@ -1,10 +1,21 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
+
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  await User.create(req.body);
+
+  return res.send();
+});
 
 app.listen(3000);
